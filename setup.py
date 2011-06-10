548a022cef8348badd73c9305fa6b5a6fdae7ec6
Ben Firshman <ben@firshman.co.uk>
2011-06-10 17:55:55 +0100

    Added manifest

--- setup.py
+++ setup.py
@@ -11,12 +11,14 @@ setup(
     url='http://github.com/bfirsh/django-ordered-model/',
     packages=[
         'ordered_model',
+        'ordered_model.tests',
     ],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Framework :: Django',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
