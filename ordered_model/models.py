fe60ae9d29827a9147abd7374797a2eaa89b174b
Aleksey G. Shalamov <mail@a-sh.org>
2013-10-18 22:29:23 +0400

    Fix for properly work with defined 'list_filter' option

--- ordered_model/models.py
+++ ordered_model/models.py
@@ -2,18 +2,19 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 
+
 class OrderedModel(models.Model):
     """
     An abstract model that allows objects to be ordered relative to each other.
     Provides an ``order`` field.
     """
-    
+
     order = models.PositiveIntegerField(editable=False)
-    
+
     class Meta:
         abstract = True
         ordering = ('order',)
-    
+
     def save(self, *args, **kwargs):
         if not self.id:
             qs = self.__class__.objects.order_by('-order')
@@ -22,9 +23,11 @@ class OrderedModel(models.Model):
             except IndexError:
                 self.order = 0
         super(OrderedModel, self).save(*args, **kwargs)
-    
-    def _move(self, up):
-        qs = self.__class__._default_manager
+
+    def _move(self, up, qs=None):
+        if qs is None:
+            qs = self.__class__._default_manager
+
         if up:
             qs = qs.order_by('-order').filter(order__lt=self.order)
         else:
@@ -37,13 +40,16 @@ class OrderedModel(models.Model):
         self.order, replacement.order = replacement.order, self.order
         self.save()
         replacement.save()
-    
+
+    def move(self, direction, qs=None):
+        self._move(direction == 'up', qs)
+
     def move_down(self):
         """
         Move this object down one position.
         """
         return self._move(up=False)
-    
+
     def move_up(self):
         """
         Move this object up one position.
