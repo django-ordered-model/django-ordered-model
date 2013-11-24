04476f76b71b9199966710fdcc03fc779411944a
Simon Charette <charette.s@gmail.com>
2013-11-24 02:38:16 -0500

    Added an `order_with_respect_to` option.

--- ordered_model/tests/models.py
+++ ordered_model/tests/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from ordered_model.models import OrderedModel
 
+
 class Item(OrderedModel):
     name = models.CharField(max_length=100)
+
+
+class Question(models.Model):
+    pass
+
+
+class Answer(OrderedModel):
+    question = models.ForeignKey(Question, related_name='answers')
+    order_with_respect_to = 'question'
+
+    class Meta:
+        ordering = ('question', 'order')
+
+    def __unicode__(self):
+        return u"Answer #%d of question #%d" % (self.order, self.question_id)
